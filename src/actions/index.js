cf1aabc4639ce918587212f5ea5cb828908fb0d9
MichaelWiss <michael.wiss@gmail.com>
2016-12-27 15:55:50 -0600

    catching data

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-const Root_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=09hj9h9h9h9hh9h';
 
 
 export function fetchPosts() {
-    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`;)
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
 	return {
 		type: FETCH_POSTS,
