b1711306b57ba384db3339e6d98feb2acef91664
MichaelWiss <michael.wiss@gmail.com>
2016-12-27 15:07:33 -0600

    request to root url

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-export function fetchPosts() {
-	return {
+const Root_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=09hj9h9h9h9hh9h';
 
-	};
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`;)
+
+	return {
+		type: FETCH_POSTS,
+		payload: request
+    };
 }
