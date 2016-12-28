8076c944a074dc9b3f3d764f50b2c9bdc1dd2538
MichaelWiss <michael.wiss@gmail.com>
2016-12-28 09:07:37 -0600

    component will mount

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 
 class PostsIndex extends Component {
+   componentWillMount() {
+   	   console.log('this would be a good time to call an action creator to fetch posts');
+   }
+
 	render() {
 		return (
 			<div>List of blog posts</div>
