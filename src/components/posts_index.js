cbe56be0f047ed36cd5eb3301b4ec34ee638268d
MichaelWiss <michael.wiss@gmail.com>
2016-12-29 13:16:01 -0600

    / in get request

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -14,7 +14,7 @@ class PostsIndex extends Component {
    		return(
           <li className="list-group-item" key={post.id}>
              <Link to={"posts/" + post.id}>
-                <span className="pull-xs-right">{post.categories}</span>
+                <span className="float-right">{post.categories}</span>
                 <strong>{post.title}</strong>
              </Link>
            </li>
