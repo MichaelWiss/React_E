999aa108fb01d96cd977a8c39145da3cf8ed272a
MichaelWiss <michael.wiss@gmail.com>
2016-12-29 16:09:59 -0600

    delete works

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -14,7 +14,7 @@ class PostsShow extends Component {
 	}
 
 	onDeleteClick() {
-		this.props.deletePost(this.props.params.id);
+		this.props.deletePost(this.props.params.id)
 		  .then(() => { this.context.router.push('/'); })
 	}
 	render() {
